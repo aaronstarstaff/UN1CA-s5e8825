3d80b7f698acf0ac313864c3ea1ce1238b50f990
aaronstarstaff <159981647+aaronstarstaff@users.noreply.github.com>
2025-05-07 18:35:36 -0700

    Update make_rom.sh
    Signed-off-by: aaronstarstaff <159981647+aaronstarstaff@users.noreply.github.com>

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -102,9 +102,6 @@ if $BUILD_ROM; then
             && bash "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/target/$TARGET_COMMON_NAME/patches"
     fi
 
-    echo -e "\n- Applying ROM mods..."
-    bash "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/unica/mods"
-
     echo -e "\n- Recompiling APKs/JARs..."
     while read -r i; do
         bash "$SRC_DIR/scripts/apktool.sh" b "$i"
